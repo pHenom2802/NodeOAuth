899d528caacb5b556cd47e31893266bb3592f034
Pankaj Yadav <pankajyadavas@gmail.com>
2016-11-30 20:15:53 +0000

    Parse cookie, body and set session

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
 var router = express.Router;
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({ secret: 'someultrasecrectSecret',
+			saveUninitialized: true,
+			resave: true
+}));
 
 var auth = express.Router();
 require('./router/auth.js')(auth);
 app.use('/auth', auth);
 
 app.get('/',function(req, res){
-	res.send('Accessing /');
+	res.send('Acessing / ' + req.session.id);
 });
 app.listen(8080);
